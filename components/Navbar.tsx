1b384a43d86c9557e7e25aa3b50c29188045252b
alferdiyan <ferdi1505@gmail.com>
2023-09-17 11:29:37 +0700

    add use client in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,18 @@
+"use client"
+
 import Image from "next/image"
 import andalan from "../public/images/andalan.png"
+import { useState } from "react"
 
 export default function Navbar() {
+    const [menuItems] = useState([
+        { id: 2, name: "About", link: "#" },
+        { id: 3, name: "Service", link: "#" },
+        { id: 4, name: "Portfolio", link: "#" },
+        { id: 5, name: "Driblle", link: "https://dribbble.com/FerdiyanDSIGN" },
+        { id: 6, name: "Behance", link: "https://www.behance.net/ferdiyanUI" },
+    ])
+
     return (
         <div className='px-10 py-3 flex justify-between bg'>
             <Image
